d18775fc579126dfb37637cd0aa40b875e3679ec
Matt Johnson <mrjz@umich.edu>
2014-02-01 11:45:35 -0800

    added initial empty template project

---
+++ PowderHunter/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PowderHunter
+//
+//  Created by Matt Johnson on 2/1/14.
+//  Copyright (c) 2014 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end

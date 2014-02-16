721ce8b8138cb97619d64fed56e70d208e1714b7
Matt Johnson <mrjz@umich.edu>
2014-02-16 11:35:18 -0800

    set ResortsViewController embedded in a UINavigationController as the root view controller

--- PowderHunter/AppDelegate.m
+++ PowderHunter/AppDelegate.m
@@ -7,13 +7,18 @@
 //
 
 #import "AppDelegate.h"
+#import "ResortsViewController.h"
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    // Override point for customization after application launch.
+
+    ResortsViewController *resortsVC = [[ResortsViewController alloc] init];
+    UINavigationController *nvc = [[UINavigationController alloc] initWithRootViewController:resortsVC];    
+    self.window.rootViewController = nvc;
+
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
